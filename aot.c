5ceada88adac3d1c660549601a011bf64fdf5fa6
Stephen Foster <67705843+Stehfyn@users.noreply.github.com>
2024-12-23 15:34:13 -0800

    my level

--- aot.c
+++ aot.c
@@ -23,16 +23,13 @@ IMAGE_DOS_HEADER __ImageBase;
 
 #ifdef _M_X64
 #define ARCH(identifier) identifier ## 64
-#define iD "AOTInstanceMutex64"
 #define SEGMENT ".shared64"
 #else
 #define ARCH(identifier) identifier ## 32
 #define SEGMENT ".shared32"
-#define iD "AOTInstanceMutex32"
 #endif
 
 #define STRINGIZE(x) #x
-
 
 #ifdef _WINDLL
 #pragma section(SEGMENT, read, write, shared)
@@ -46,19 +43,20 @@ ALLOC_SEGMENT(SEGMENT, DWORD,     dwAotProcessId,     0);
 
 #define AOT_WINDOW_NAME            (TEXT("AOT"))
 #define AOT_CLASS_NAME             (TEXT("AOTClass"))
-#define AOT_INSTANCE_MUTEX         (TEXT(iD))
+#define AOT_INSTANCE_MUTEX         (TEXT(STRINGIZE(ARCH(AOTInstanceMutex))))
 
+#define SYSCOMMAND(value)          (value & 0xFFF0)
 #define AOT_MENU_ALWAYS_ON_TOP     (0x69)
 #define AOT_MENU_EXIT              (0x69 + 1)
+#define SC_AOT                     (SYSCOMMAND(AOT_MENU_ALWAYS_ON_TOP))
 
 #define AOT_MENUTEXT_ALWAYS_ON_TOP (TEXT("Always On Top"))
 #define AOT_MENUTEXT_EXIT          (TEXT("Exit"))
 
-#define WM_AOTEXIT                 (WM_USER + 0x69)
+#define WM_AOTQUIT                 (WM_USER + 0x69)
 #define WM_AOTHOOKINIT             (WM_USER + 0x69 + 1)
 #define WM_AOTMOUSEHOOK            (WM_USER + 0x69 + 2)
 #define WM_AOTCBTHOOK              (WM_USER + 0x69 + 3)
-
 #define HANDLE_WM_AOTHOOKINIT(hWnd, wParam, lParam, fn) \
         ((fn)((hWnd), (AOTHOOKTHREAD)(wParam), (DWORD)(lParam)), 0L)
 
@@ -186,7 +184,6 @@ OnAotInit(
   DWORD         dwAotHookThreadId)
 {
   LPAOTUSERDATA lpAotUserData = (LPAOTUSERDATA)GetWindowLongPtr(hWnd, GWLP_USERDATA);
-
   switch (dwHookThread) {
   case AOTMOUSEHOOKTHREAD: {
     wprintf(L"AotMouseHookThreadInit %ld\n", dwAotHookThreadId);
@@ -214,22 +211,28 @@ OnAotMouseHook(
 {
   UNREFERENCED_PARAMETER(hWnd);
   UNREFERENCED_PARAMETER(mouseData);
-  HWND  hWndHit = NULL;
-  POINT pt      = {0};
+
+  POINT pt;
+  HWND  hwndHovered;
+
+  RtlSecureZeroMemory(&pt, sizeof(POINT));
   POINTSTOPOINT(pt, MAKEPOINTS(pts));
-  hWndHit = WindowFromPoint(pt);
 
-  if (hWndHit && GetKeyState(VK_LCONTROL) < 0)
+  RtlSecureZeroMemory(&hwndHovered, sizeof(HWND));
+  hwndHovered = WindowFromPoint(pt);
+
+  if(hwndHovered)
   {
-    //HMENU hMenu = GetSystemMenu(hWndHit, TRUE);
-//(void)hMenu;
-    //HMENU hMenu = (HMENU)FindOpenMenuWindow();
-    //if (hMenu)
-    //{
-    //  wprintf(L"Menu: %ld\n", (LONG)hMenu);
-    //  return;
-    //}
-
+    TCHAR   szClassName[256];
+    //LRESULT result = SendMessage(hwndHovered, WM_NCHITTEST, 0, MAKELPARAM(pt.x, pt.y));
+    RtlSecureZeroMemory(szClassName, sizeof(szClassName));
+    if(GetClassName(hwndHovered, szClassName, sizeof(szClassName)))
+    {
+      TCHAR szWindowText[256];
+      RtlSecureZeroMemory(szWindowText, sizeof(szWindowText));
+      GetWindowText(hwndHovered, szWindowText, sizeof(szWindowText));
+      //wprintf(L"%ld %ld HT: %ld: | %s %s\n", pt.x, pt.y, (LONG)result, szWindowText, szClassName);
+    }
   }
 }
 
@@ -244,6 +247,7 @@ GetCbtMessageName(
   case HCBT_ACTIVATE: return _T("HCBT_ACTIVATE");
   case HCBT_CREATEWND: return _T("HCBT_CREATEWND");
   case HCBT_DESTROYWND: return _T("HCBT_DESTROYWND");
+  case HCBT_SYSCOMMAND: return _T("HCBT_SYSCOMMAND");
   default: return _T("");
   }
 }
@@ -260,10 +264,13 @@ OnAotCbtHook(
   static int i = 0;
   UNREFERENCED_PARAMETER(hWnd);
   TCHAR szClassName[256];
+
   if(GetClassName(hWnd2, szClassName, ARRAYSIZE(szClassName)))
   {
-    _tprintf(_T("%d AotCbtHook %s Context Menu %s\n"), i++, GetCbtMessageName(nCode), szClassName);
-
+    
+    TCHAR szName[256];
+    GetWindowText(hWnd2, szName, sizeof(szName));
+    _tprintf(_T("%d AotCbtHook %s\n%s\n%s\n"), i++, GetCbtMessageName(nCode), szName, szClassName);
     if(GetKeyState(VK_LCONTROL) < 0)
     {
       POINT cursor = {0};
@@ -281,12 +288,15 @@ OnAotCbtHook(
       HWND  hWndForeground        = GetForegroundWindow();
       DWORD windowThreadProcessId = GetWindowThreadProcessId(hWndForeground, (LPDWORD)0);
       DWORD currentThreadId       = GetCurrentThreadId();
-      DWORD CONST_SW_SHOW         = 5;
       LogWindowText(hWndForeground);
       AttachThreadInput(windowThreadProcessId, currentThreadId, TRUE);
-      BringWindowToTop(hWnd);
-      ShowWindow(hWnd, CONST_SW_SHOW);
+      LockSetForegroundWindow(LSFW_UNLOCK);
+      AllowSetForegroundWindow(ASFW_ANY);
+      SetForegroundWindow(hWnd);
+      LockSetForegroundWindow(LSFW_LOCK);
+      ShowWindow(hWnd, SW_SHOW);
       AttachThreadInput(windowThreadProcessId, currentThreadId, FALSE);
+      //AttachThreadInput(currentThreadId, windowThreadProcessId, TRUE);
       INT nMenuId = (INT)TrackPopupMenu(
         hPopupMenu,
         TPM_LEFTALIGN | TPM_LEFTBUTTON | TPM_RETURNCMD | TPM_NONOTIFY,
@@ -296,6 +306,7 @@ OnAotCbtHook(
         hWnd,
         NULL
       );
+      LockSetForegroundWindow(LSFW_UNLOCK);
       if (nMenuId)
       {
         switch (nMenuId)
@@ -310,19 +321,20 @@ OnAotCbtHook(
             0,
             0,
             0,
-            SWP_NOMOVE | SWP_NOSIZE
+            SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW | SWP_FRAMECHANGED
           );
           break;
         }
         case AOT_MENU_EXIT:
         {
           LPAOTUSERDATA lpAot = (LPAOTUSERDATA)GetWindowLongPtr(hWnd, GWLP_USERDATA);
-          PostThreadMessage(lpAot->dwMouseHookThreadId, WM_AOTEXIT, 0, 0);
+          PostThreadMessage(lpAot->dwMouseHookThreadId, WM_AOTQUIT, 0, 0);
           PostQuitMessage(0);
           break;
         }
         }
       }
+
       DestroyMenu(hPopupMenu);
     }
     //if(!_tccmp(szClassName, _T("32768")))
@@ -338,6 +350,7 @@ CALLBACK
 OnClose(
   HWND hWnd)
 {
+  UnsetCbtHook();
   DestroyWindow(hWnd);
 }
 
@@ -349,11 +362,13 @@ OnDestroy(
   HWND hWnd)
 {
   LPAOTUSERDATA lpAotUserData = (LPAOTUSERDATA)GetWindowLongPtr(hWnd, GWLP_USERDATA);
+  UnsetCbtHook();
+  SendNotifyMessage(HWND_BROADCAST, WM_NULL, 0, 0);
 
   if (lpAotUserData->dwCbtHookThreadId)
   {
     HANDLE hCbtHookThread = OpenThread(SYNCHRONIZE, FALSE, lpAotUserData->dwCbtHookThreadId);
-    PostThreadMessage(lpAotUserData->dwCbtHookThreadId, WM_AOTEXIT, 0, 0);
+    PostThreadMessage(lpAotUserData->dwCbtHookThreadId, WM_AOTQUIT, 0, 0);
 
     if (hCbtHookThread)
     {
@@ -365,7 +380,7 @@ OnDestroy(
   if (lpAotUserData->dwMouseHookThreadId)
   {
     HANDLE hMouseHookThread = OpenThread(SYNCHRONIZE, FALSE, lpAotUserData->dwMouseHookThreadId);
-    PostThreadMessage(lpAotUserData->dwMouseHookThreadId, WM_AOTEXIT, 0, 0);
+    PostThreadMessage(lpAotUserData->dwMouseHookThreadId, WM_AOTQUIT, 0, 0);
 
     if (hMouseHookThread)
     {
@@ -374,8 +389,6 @@ OnDestroy(
     }
   }
 
-  //printf("UnsetHook()\n");
-  UnsetCbtHook();
   PostQuitMessage(0);
 }
 
@@ -388,18 +401,91 @@ LowLevelMouseProc(
   WPARAM wParam,
   LPARAM lParam)
 {
-  if (WM_RBUTTONUP == wParam) {
+  switch (wParam) {
+  case WM_MOUSEMOVE:
     PostThreadMessage(
       GetCurrentThreadId(),
       WM_AOTMOUSEHOOK,
-      (WPARAM) (DWORD) HIWORD(((LPMSLLHOOKSTRUCT)lParam)->mouseData),
-      (LPARAM) (DWORD) POINTTOPOINTS(((LPMSLLHOOKSTRUCT)lParam)->pt)
+      (WPARAM)(DWORD)HIWORD(((LPMSLLHOOKSTRUCT)lParam)->mouseData),
+      (LPARAM)(DWORD)POINTTOPOINTS(((LPMSLLHOOKSTRUCT)lParam)->pt)
     );
+  default:
+    return CallNextHookEx(NULL, nCode, wParam, lParam);
   }
-  return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
 #ifdef _WINDLL
+
+static 
+CFORCEINLINE
+VOID
+CALLBACK
+AppendSystemMenu(HWND hWnd)
+{
+  HMENU hSysMenu = GetSystemMenu(hWnd, FALSE);
+  if(hSysMenu)
+  {
+    MENUITEMINFO mii;
+    RtlSecureZeroMemory(&mii, sizeof(MENUITEMINFO));
+    mii.cbSize = sizeof(MENUITEMINFO);
+    mii.fMask  = MIIM_ID;
+    if (GetMenuItemInfo(hSysMenu, AOT_MENU_ALWAYS_ON_TOP, FALSE, &mii))
+      return;
+    AppendMenu(
+      hSysMenu,
+      MF_BYPOSITION | MF_STRING,
+      AOT_MENU_ALWAYS_ON_TOP,
+      AOT_MENUTEXT_ALWAYS_ON_TOP
+    );
+  }
+}
+
+static
+CFORCEINLINE
+VOID 
+CALLBACK
+Sentinel(LPVOID lpvHwnd) 
+{
+  UNREFERENCED_PARAMETER(lpvHwnd);
+  HANDLE hProcessId = OpenProcess(SYNCHRONIZE, FALSE, ARCH(dwAotProcessId));
+  if (hProcessId)
+  {
+    WaitForSingleObject(hProcessId, INFINITE);
+  }
+  SendNotifyMessage(HWND_BROADCAST, WM_NULL, 0, 0);
+}
+
+static 
+CFORCEINLINE
+LRESULT
+CALLBACK
+Subclassproc(
+  HWND      hWnd,
+  UINT      uMsg,
+  WPARAM    wParam,
+  LPARAM    lParam,
+  UINT_PTR  uIdSubclass,
+  DWORD_PTR dwRefData)
+{
+  UNREFERENCED_PARAMETER(dwRefData);
+  switch (uMsg) {
+  case WM_INITMENUPOPUP:
+  {
+    //CloseHandle(CreateThread(0, 0, (LPTHREAD_START_ROUTINE)Sentinel, NULL, 0, 0));
+    
+    AppendMenu(
+      (HMENU)wParam,
+      MF_BYPOSITION | MF_STRING,
+      AOT_MENU_ALWAYS_ON_TOP,
+      AOT_MENUTEXT_ALWAYS_ON_TOP
+    );
+  }
+  case WM_NCDESTROY:
+    RemoveWindowSubclass(hWnd, Subclassproc, uIdSubclass);
+  default:
+    return DefSubclassProc(hWnd, uMsg, wParam, lParam);
+  }
+}
 
 static
 CFORCEINLINE
@@ -411,7 +497,70 @@ CBTProc(
   LPARAM lParam)
 {
   switch (nCode) {
+  case HCBT_SYSCOMMAND:
+  {
+    switch(wParam) {
+    case SC_AOT:
+    {
+      HWND  hWnd    = GetForegroundWindow();
+      DWORD exStyle = (DWORD)GetWindowLong(hWnd, GWL_EXSTYLE);
+      (VOID)SetWindowPos(
+        hWnd,
+        (exStyle & WS_EX_TOPMOST) ? HWND_NOTOPMOST : HWND_TOPMOST,
+        0,
+        0,
+        0,
+        0,
+        SWP_NOMOVE|SWP_NOSIZE|SWP_SHOWWINDOW|SWP_FRAMECHANGED
+      );
+      PostMessage(
+        ARCH(hWndHookMarshaller),
+        WM_AOTCBTHOOK,
+        (WPARAM)(DWORD)nCode,
+        (LPARAM)(LONG_PTR)hWnd
+      );
+    }
+    default:
+      break;
+    }
+    
+    break;
+  }
+  //case HCBT_CREATEWND:
   case HCBT_ACTIVATE:
+  {
+    if (lParam)
+    {
+      //HWND hwndInsertAfter = ((LPCBT_CREATEWND)lParam)->hwndInsertAfter;
+      HWND hWndActive = ((LPCBTACTIVATESTRUCT)lParam)->hWndActive;
+      //if(hwndInsertAfter)
+      if(hWndActive)
+      {
+        //if(hwndInsertAfter == GetActiveWindow())
+        {
+          TCHAR szClassName[256];
+          GetClassName((HWND)wParam, szClassName, sizeof(szClassName));
+          if(_tccmp(szClassName, _T("#32768")))
+          {
+            //(void)GetSystemMenu((HWND)wParam, FALSE);
+            //SetWindowSubclass((HWND)wParam, Subclassproc, 0, (DWORD_PTR)GetCurrentProcessId());
+            //(void)GetSystemMenu((HWND)wParam, FALSE);
+            //HMENU hMenu = GetSystemMenu((HWND)wParam, FALSE);
+            AppendSystemMenu((HWND)wParam);
+
+            PostMessage(
+              ARCH(hWndHookMarshaller),
+              WM_AOTCBTHOOK,
+              (WPARAM)(DWORD)nCode,
+              (LPARAM)(LONG_PTR)wParam
+            );
+          }
+        }
+      }
+    }
+    break;
+  }
+  //case HCBT_ACTIVATE:
   case HCBT_CREATEWND:
   case HCBT_DESTROYWND:
   {
@@ -421,7 +570,6 @@ CBTProc(
       (WPARAM)(DWORD)nCode,
       (LPARAM)(LONG_PTR)wParam
     );
-    break;
   }
   default:
     break;
@@ -461,7 +609,8 @@ UnsetCbtHook(
     ARCH(hWndHookMarshaller) = NULL;
     dwExitCode               = EXIT_SUCCESS;
   }
-  FreeLibraryAndExitThread(ARCH(hCbtHookModule), dwExitCode);
+  return (BOOL)dwExitCode;
+  //FreeLibraryAndExitThread(ARCH(hCbtHookModule), dwExitCode);
 }
 
 
@@ -471,7 +620,7 @@ static
 CFORCEINLINE
 LRESULT
 CALLBACK
-WndProc(
+AotWndProc(
   HWND   hWnd,
   UINT   message,
   WPARAM wParam,
@@ -504,7 +653,7 @@ AotMouseHookThread(
 
   if (hook)
   {
-    MSG msg = {0};
+    MSG msg;
 
     PostMessage(
       *(HWND*)lphWnd,
@@ -513,23 +662,26 @@ AotMouseHookThread(
       (LPARAM)(DWORD)GetCurrentThreadId()
     );
 
+    RtlSecureZeroMemory(&msg, sizeof(MSG));
     while (GetMessage(&msg, NULL, 0, 0)) {
       TranslateMessage(&msg);
       DispatchMessage(&msg);
-      if (WM_AOTEXIT == msg.message) break;
 
       switch (msg.message) {
+      case WM_QUIT:
+      case WM_AOTQUIT:
+        UnhookWindowsHookEx(hook);
+        SendNotifyMessage(HWND_BROADCAST, WM_NULL, 0, 0);
+        break;
       case WM_AOTMOUSEHOOK:
         PostMessage(*(HWND*)lphWnd, msg.message, msg.wParam, msg.lParam);
       default:
         continue;
       }
+      break;
     }
-
-    UnhookWindowsHookEx(hook);
     return EXIT_SUCCESS;
   }
-
   return EXIT_FAILURE;
 }
 
@@ -542,25 +694,44 @@ AotCbtHookThread(
 {
   if (SetCbtHook(*(HWND*)lphWnd))
   {
-    MSG msg = {0};
+    MSG msg;
 
     PostMessage(
       *(HWND*)lphWnd,
       WM_AOTHOOKINIT,
-      (WPARAM) (DWORD) AOTCBTHOOKTHREAD,
-      (LPARAM) (DWORD) GetCurrentThreadId()
+      (WPARAM)(DWORD)AOTCBTHOOKTHREAD,
+      (LPARAM)(DWORD)GetCurrentThreadId()
     );
 
+    RtlSecureZeroMemory(&msg, sizeof(MSG));
     while (GetMessage(&msg, NULL, 0, 0)) {
       TranslateMessage(&msg);
       DispatchMessage(&msg);
-      if (WM_AOTEXIT == msg.message) break;
-    }
 
+      switch (msg.message) {
+      case WM_QUIT:
+      case WM_AOTQUIT:
+        UnsetCbtHook();
+        SendNotifyMessage(HWND_BROADCAST, WM_NULL, 0, 0);
+        break;
+      default:
+        continue;
+      }
+      break;
+    }
     return EXIT_SUCCESS;
   }
-
   return EXIT_FAILURE;
+}
+
+static CFORCEINLINE
+BOOL WINAPI
+HandlerRoutine(
+  DWORD dwCtrlType)
+{
+  UNREFERENCED_PARAMETER(dwCtrlType);
+  UnsetCbtHook();
+  return TRUE;
 }
 
 static
@@ -579,6 +750,10 @@ CreateConsole(
   {
     return FALSE;
   }
+  //if(!SetConsoleCtrlHandler((PHANDLER_ROUTINE)HandlerRoutine, TRUE))
+  //{
+  //  return FALSE;
+  //}
   if (!SetConsoleTitle(_T("AOT Console")))
   {
     return FALSE;
@@ -653,8 +828,8 @@ Watchdog(
 //Sleep(100);
     //FreeLibraryAndExitThread(ARCH(hCbtHookModule), EXIT_SUCCESS);
   }
-  //FreeLibrary(ARCH(hCbtHookModule));
-  FreeLibraryAndExitThread(GetModuleHandle(_T("aot64.dll")), 0); //Update the refcount first
+  //FreeLibrary(GetModuleHandle(NULL));
+  //FreeLibraryAndExitThread(GetModuleHandle(_T("aotx64.dll")), 0); //Update the refcount first
 }
 
 BOOL
@@ -666,33 +841,16 @@ DllMain(
 {
   UNREFERENCED_PARAMETER(hModule);
   UNREFERENCED_PARAMETER(lpReserved);
-  switch (ul_reason_for_call)
-  {
+  switch (ul_reason_for_call) {
   case DLL_PROCESS_ATTACH:
   {
-    if(!ARCH(dwAotProcessId))
-    {
-      ARCH(dwAotProcessId) = GetCurrentProcessId();
-    }
-    //else
-    //{
-    //  if(!CreateThread(
-    //       0,
-    //       0,
-    //       (LPTHREAD_START_ROUTINE)Watchdog,
-    //       NULL,
-    //       0,
-    //       0
-    //  ))
-    //  {
-    //    return FALSE;
-    //  }
-    //}
+    if(!ARCH(dwAotProcessId)) ARCH(dwAotProcessId) = GetCurrentProcessId();
     ARCH(hCbtHookModule) = hModule;
-    //DisableThreadLibraryCalls(hModule);
-    break;
+    DisableThreadLibraryCalls(hModule);
   }
-  default: break;
+  default:
+    SendNotifyMessage(HWND_BROADCAST, WM_NULL, 0, 0);
+    break;
   }
   return TRUE;
 }
@@ -702,24 +860,28 @@ int
 _tmain(
   void)
 {
-  AOTUSERDATA aot  = {0};
-  WNDCLASSEX  wcx  = { sizeof(WNDCLASSEX) };
-  HWND        hWnd = NULL;
+  AOTUSERDATA aot;
+  WNDCLASSEX  wcx;
+  HWND        hWnd;
 
   if(S_OK != SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE))
   {
     return EXIT_FAILURE;
   }
 
-  wcx.lpfnWndProc   = WndProc;
+  RtlSecureZeroMemory(&wcx, sizeof(WNDCLASSEX));
+  wcx.cbSize        = sizeof(WNDCLASSEX);
   wcx.hInstance     = (HINSTANCE)&__ImageBase;
   wcx.lpszClassName = AOT_CLASS_NAME;
+  wcx.lpfnWndProc   = AotWndProc;
 
   if (!(ATOM)RegisterClassEx(&wcx))
   {
     return EXIT_FAILURE;
   }
 
+  RtlSecureZeroMemory(&hWnd, sizeof(HWND));
+  RtlSecureZeroMemory(&aot, sizeof(AOTUSERDATA));
   hWnd = CreateWindowEx(
     WS_EX_TOOLWINDOW,
     AOT_CLASS_NAME,
@@ -749,15 +911,12 @@ _tmain(
   {
     if (CreateThread(0, 0, (LPTHREAD_START_ROUTINE)AotCbtHookThread, (LPVOID)&hWnd, 0, 0))
     {
-      for (;;)
-      {
-        MSG msg = {0};
-        if (GetMessage(&msg, NULL, 0U, 0U)) {
-          TranslateMessage(&msg);
-          DispatchMessage(&msg);
-          continue;
-        }
-        break;
+      MSG msg;
+      RtlSecureZeroMemory(&msg, sizeof(MSG));
+      while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+        continue;
       }
       return EXIT_SUCCESS;
     }
--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-::echo off
+@echo off
 setlocal EnableDelayedExpansion
 cd "%~dp0"
 
@@ -13,36 +13,33 @@ if not defined VSINSTALLPATH (
 
 set out=out
 set sources=..\..\aot.c
-set flags=/nologo /O2 /Oi  /std:c11 /Wall /wd5045 /wd4710 /wd4191 /WX /D _NDEBUG /D UNICODE /D _UNICODE
+set cflags=/nologo /O2 /Oi /MD /std:c11 /Wall /WX /wd5045 /wd4710 /wd4191 /D _NDEBUG /D UNICODE /D _UNICODE
 set libs=user32.lib comctl32.lib shcore.lib ntdll.lib Kernel32.lib vcruntime.lib ucrt.lib Shlwapi.lib Comctl32.lib
 
-rmdir /s /q %out%
-mkdir %out%
-pushd %out%
+set "build_target=/?"
 
-set arch=x86
 if exist "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" (
-    call "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" %arch%
-    echo %arch% dll
-    rmdir /s /q %arch% 2>nul1
-    mkdir %arch%
-    pushd %arch%
-    cl /MD %flags% /D "_USRDLL" /D "_WINDLL" /LD %sources% /link /MACHINE:X86 %libs% /IMPLIB:aot32.lib /OUT:aot32.dll /ENTRY:DllMain
-    echo i386 exe
-    cl /MD %flags% /Tc %sources% %libs% aot32.lib /link /MACHINE:X86
-    popd
+	rmdir /s /q %out%
+	mkdir %out%
+	pushd %out%
+
+	call :%%build_target%% x86 aot 3>&1 >nul
+	call :%%build_target%% x64 aot 3>&1 >nul
+
+	popd
 )
-set arch=x64
-if exist "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" (
-    call "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" %arch%
-    rmdir /s /q %arch% 2>&1>nul
-    mkdir %arch%
-    pushd %arch%
-    echo %arch% dll
-    cl /MD %flags% /D "_USRDLL" /D "_WINDLL" /LD %sources% /link %libs% /IMPLIB:aot64.lib /OUT:aot64.dll /ENTRY:DllMain
-    echo %arch% exe
-    cl /MD %flags% /Tc %sources% %libs% aot64.lib 
-    popd
-)
+
+if "%0" == ":%build_target%" (
+	echo Build Target %2-%1 @call:
+	call "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" %1
+	echo %1 dll
+	rmdir /s /q %1 2>nul
+	mkdir %1
+	pushd %1
+	cl %cflags% /D "_WINDLL" /LD %sources% /link /MACHINE:%1 %libs% /IMPLIB:%2%1.lib /OUT:%2%1.dll /ENTRY:DllMain
+	echo %1 exe
+	cl %cflags% /Tc %sources% %libs% %2%1.lib /link /MACHINE:%1
+	popd
+)>&3
 
 exit /b 0
